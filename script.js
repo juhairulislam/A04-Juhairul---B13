d6eeb5263ecc4aa23ba617b008985d98fa163759
juhairulislam <juhairulislam2018@gmail.com>
2026-02-25 16:22:20 +0600

    made this project completely defferent approch

--- script.js
+++ script.js
@@ -1,90 +1,162 @@
-let total = document.getElementById('total') ;
-let interviewCount = document.getElementById('interview-count');
-let rejectedCount = document.getElementById('rejected-count') ;
+let currentTab = 'all';
+
+const tabActive = ['bg-[#3B82F6]', 'text-white'];
+const tabInactive = ['bg-transparent', 'text-[#64748B]'];
 
 
 
+const allContainer = document.getElementById("all-container");
+const interviewContainer = document.getElementById('interview-container');
+const rejectedContainer = document.getElementById('reject-container');
+const emptyStat = document.getElementById('no-jobs-available')
 
 
 
+function switchTab(tab) {
 
+    const tabs = ['all', 'interview', 'rejected'];
+    currentTab = tab ;
 
-const allBtn = document.getElementById('all-showing-btn') ;
-const interviewShowingBtn = document.getElementById('interview-showing-btn');
-const rejectedShowingBtn = document.getElementById('rejected-showing-btn') ;
-const allCards = document.getElementById('allCards')
-const noJobAvailable = document.getElementById('no-jobs-available') ;
+    for (const t of tabs) {
 
+        const tabName = document.getElementById('tab-' + t);
 
-// count 
+        if (t === tab) {
+            tabName.classList.remove(...tabInactive);
+            tabName.classList.add(...tabActive)
+        } else {
+            tabName.classList.remove(...tabActive);
+            tabName.classList.add(...tabInactive)
+        }
 
-function calculateCount(){
-    total.innerText = allCards.children.length ;
+    }
 
-   
-}
+    const pages = [allContainer, interviewContainer, rejectedContainer];
 
-calculateCount()
+    for (const section of pages) {
 
+        section.classList.add('hidden')
 
+    }
 
+    emptyStat.classList.add('hidden')
 
+    if (tab === 'all') {
+        allContainer.classList.remove('hidden');
+        if(allContainer.children.length < 1){
+                emptyStat.classList.remove('hidden')
 
-function noJobsAvailable(id){
+        }
+    }
+    else if (tab === 'interview') {
 
-    noJobAvailable.classList.remove('hidden')
+        interviewContainer.classList.remove('hidden') ;
 
-    allCards.classList.add('hidden') ;
+        if(interviewContainer.children.length < 1){
+                emptyStat.classList.remove('hidden')
 
+        }
 
-    allBtn.classList.remove('bg-[#3B82F6]', 'text-white');
-    interviewShowingBtn.classList.remove('bg-[#3B82F6]', 'text-white');
-    rejectedShowingBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+    }
+    else {
+        rejectedContainer.classList.remove('hidden') ;
 
+        if( rejectedContainer.children.length < 1){
+                emptyStat.classList.remove('hidden')
 
-    allBtn.classList.add('text-[#64748B]');
-    interviewShowingBtn.classList.add('text-[#64748B]');
-    rejectedShowingBtn.classList.add('text-[#64748B]');
+        }
+    }
 
-
-    const selected = document.getElementById(id) ;
-
-    selected.classList.remove('text-[#64748B]') ;
-    selected.classList.add('bg-[#3B82F6]', 'text-white') ;
-     totalJobCount.innerText =  `${interviewStore.children.length} Jobs` ;
-      document.getElementById('all-showing-btn').addEventListener('click' , function(){
-            
-            totalJobCount.innerText = `${Number(total.innerText)} Jobs `
-        })
-
-    
-}
-
-function allJobsShowing(id){
-
-     noJobAvailable.classList.add('hidden')
-
-    allCards.classList.remove('hidden') ;
-
-
-     allBtn.classList.remove('bg-[#3B82F6]', 'text-white');
-    interviewShowingBtn.classList.remove('bg-[#3B82F6]', 'text-white');
-    rejectedShowingBtn.classList.remove('bg-[#3B82F6]', 'text-white');
-
-
-    allBtn.classList.add('text-[#64748B]');
-    interviewShowingBtn.classList.add('text-[#64748B]');
-    rejectedShowingBtn.classList.add('text-[#64748B]');
-
-
-
-     const selected = document.getElementById(id) ;
-
-    selected.classList.remove('text-[#64748B]')
-    selected.classList.add('bg-[#3B82F6]', 'text-white')
+    updateStat() ;
 
 }
+  
+
+// stat update
+const totalCount = document.getElementById('total-count');
+const interviewCount = document.getElementById('interview-count');
+const rejectedCount = document.getElementById('rejected-count');
+const availableStat = document.getElementById('available')
+
+
+switchTab(currentTab)
+
+
+
+document.getElementById('job-container').addEventListener('click', function (event) {
+
+    const clickedElement = event.target;
+
+    const card = clickedElement.closest('.card');
+    const parent = card.parentNode ;
+
+    const status = card.querySelector('#status')
+
+
+
+    if (clickedElement.classList.contains('interview')) {
+
+        status.innerText = 'INTERVIEW' ;
+
+        interviewContainer.appendChild(card);
+
+        updateStat() ;
+
+    }
+    if (clickedElement.classList.contains('rejected')) {
+
+
+        status.innerText = 'REJECTED' ;
+
+        rejectedContainer.appendChild(card);
+        updateStat() ;
+    }
+    if (clickedElement.classList.contains('delete')) {
+
+        alert('Are You Sure ?') ;
+
+        parent.removeChild(card) ;
+
+        updateStat() ;
+
+
+    }
+
+})
+
+
+
+// update count 
+
+function updateStat(){
+
+    totalCount.innerText = allContainer.children.length;
+    interviewCount.innerText = interviewContainer.children.length;
+    rejectedCount.innerText = rejectedContainer.children.length;
+
+    const counts = {
+        all : allContainer.children.length ,
+        interview : interviewContainer.children.length ,
+        rejected : rejectedContainer.children.length ,
+    } ;
+
+    totalCount.innerText = counts['all'] ;
+     interviewCount.innerText = counts['interview'] ;
+
+      rejectedCount.innerText = counts['rejected'] ;
+      availableStat.innerText = counts[currentTab] ;
+
+      if(counts[currentTab] < 1){
+
+         emptyStat.classList.remove('hidden')
+
+      } else{
+         emptyStat.classList.add('hidden')
+      }
 
 
 
 
+}
+
+updateStat() ;
